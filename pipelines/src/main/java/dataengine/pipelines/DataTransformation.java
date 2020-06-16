51e623c8b79c3f93cfddd9c1698a134b353d3be0
Gabe <1545350+gabrielenizzoli@users.noreply.github.com>
2020-06-16 11:14:03 -0700

    now it is possible to define sql as a transformation
    spark logical plan get replanned so that it does not require a sql statement to be declared in the catalog

--- pipelines/src/main/java/dataengine/pipelines/DataTransformation.java
+++ pipelines/src/main/java/dataengine/pipelines/DataTransformation.java
@@ -1,8 +1,17 @@
 package dataengine.pipelines;
 
+import dataengine.pipelines.transformations.Transformation;
 import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Encoder;
 
 import java.util.function.Function;
 
-public interface DataTransformation<S, D> extends Function<Dataset<S>, Dataset<D>> {
+public interface DataTransformation<S, D> {
+
+    Dataset<D> apply(Dataset<S> sDataset);
+
+    default <D2> DataTransformation<S, D2> andThenEncode(Encoder<D2> encoder) {
+        return s -> Transformation.<D, D2>encode(encoder).apply(apply(s));
+    }
+
 }
