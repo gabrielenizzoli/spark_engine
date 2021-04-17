c5f82f7acf20e3aa1fc434ba62afaa96329e888f
Gabriele Nizzoli <1545350+gabrielenizzoli@users.noreply.github.com>
2021-04-17 11:24:58 -0700

    localhost binding for tests

--- spark-test/src/main/java/sparkengine/spark/test/SparkSessionManager.java
+++ spark-test/src/main/java/sparkengine/spark/test/SparkSessionManager.java
@@ -19,9 +19,6 @@ public class SparkSessionManager {
                 .config("spark.sql.streaming.forceDeleteTempCheckpointLocation", true)
                 .config("spark.driver.host", "127.0.0.1")
                 .config("spark.driver.bindAddress", "127.0.0.1")
-                .config("spark.ui.enabled", false)
-                .config("spark.ui.port", 8080)
-                .config("spark.port.maxRetries", 160)
                 .getOrCreate();
     }
 
